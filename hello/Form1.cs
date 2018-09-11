a27a19fe0240a85907a5caf68ea6103df26e1095
rc0033515 <rc0033515@CRO-24-01>
2018-09-11 09:44:41 +0100

    text hello

--- hello/Form1.cs
+++ hello/Form1.cs
@@ -19,7 +19,12 @@ namespace hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello");
+            MessageBox.Show("hello " + txtbxname.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
